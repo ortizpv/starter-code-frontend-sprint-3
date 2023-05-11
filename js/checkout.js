d1190e81da8843aee681b6ecea057d0ce395f43f
ortizpv <ortizveruska@gmail.com>
2023-05-11 10:45:52 +0200

    Validaciones usuarios sin probar

--- js/checkout.js
+++ js/checkout.js
@@ -26,3 +26,67 @@ function validate() {
 	}
 
 }
+
+	// validaciones usuario
+function validate() {
+	let error = 0;
+
+	// Get the input fields
+	const fEmail = document.getElementById("fEmail");
+	const fAddress = document.getElementById("fAddress");
+	const fLastN = document.getElementById("fLastN");
+	const fPassword = document.getElementById("fPassword");
+	const fPhone = document.getElementById("fPhone");
+
+	// Get the error elements
+	let errorName = document.getElementById("errorName");
+	let errorEmail = document.getElementById("errorEmail");
+	let errorAddress = document.getElementById("errorAddress");
+	let errorLastN = document.getElementById("errorLastN");
+	let errorPassword = document.getElementById("errorPassword");
+	let errorPhone = document.getElementById("errorPhone");
+
+	// Validate fields entered by the user: name, phone, password, and email
+	debugger
+
+	const isValidPassword = fPassword.checkValidity()
+	const isValidPhone = fPhone.checkValidity()
+	const isValidLastN = fLastN.checkValidity()
+	const isValidEmail = fEmail.checkValidity()
+	const isValidName = fName.checkValidity()
+	const isValidAdress = fAddress.checkValidity()
+
+
+	if(fName.value == "" || isValidName == false){
+		return error++;
+	}
+
+	if(fLastN.value == "" || isValidLastN == false){
+		error++;
+	}
+
+	if(fEmail.value == "" || isValidEmail == false){
+		error++;
+	}
+
+	if(fAddress.value == "" || isValidAdress == false){
+		error++;
+	}
+
+
+
+	if(fPassword.value == "" || isValidPassword == false){
+		error++;
+	}
+
+	if(fPhone.value == "" || isValidPhone == false){
+		error++;
+	}
+
+	if(error>0){
+		alert("Error");
+	}else{
+		alert("OK");
+	}
+
+}
--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,5 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
+const products = [
    {
         id: 1,
         name: 'cooking oil',
@@ -64,33 +64,51 @@ var products = [
     }
 ]
 // Array with products (objects) added directly with push(). Products in this array are repeated.
-var cartList = [];
+const cartList = [];
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
-var cart = [];
+const cart = [];
 
-var total = 0;
+const total = 0;
 
 // Exercise 1
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
-}
+    for (let i = 0; i < products.length; i++) {
+        if (cart.push(products[i].id)) {
+            cartList.push(products[i]);
+        }
+        console.log("cartList", cartList);
+        generateCart();
+
+    }
+
 
 // Exercise 2
 function cleanCart() {
 
 }
 
+
 // Exercise 3
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
+    for (item of cartList) {
+        total += item.price;
+      }
+      console.log(total);
 }
 
 // Exercise 4
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+    cartList[];
+
+
+
+
 }
 
 // Exercise 5
