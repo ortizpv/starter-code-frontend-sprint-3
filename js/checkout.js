a0aca9247cc45cb4daced88d12bdadc63d040c33
ortizpv <ortizveruska@gmail.com>
2023-05-15 11:52:48 +0200

    Terminado, aún tengo 3 errores.

--- js/checkout.js
+++ js/checkout.js
@@ -1,82 +1,57 @@
 
 // Exercise 6
-function validate() {
-	var error = 0;
-	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
-
-	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
-	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
-		error++;
-	}
-
-	if(fEmail.value == ""){
-		error++;
-	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
-	}
-
-}
 
 	// validaciones usuario
 function validate() {
 	let error = 0;
 
 	// Get the input fields
-	const fEmail = document.getElementById("fEmail");
-	const fAddress = document.getElementById("fAddress");
+	const fName = document.getElementById("fName");
 	const fLastN = document.getElementById("fLastN");
-	const fPassword = document.getElementById("fPassword");
+	const fAddress = document.getElementById("fAddress");
 	const fPhone = document.getElementById("fPhone");
+	const fEmail = document.getElementById("fEmail");
+	const fPassword = document.getElementById("fPassword");
+	
 
 	// Get the error elements
 	let errorName = document.getElementById("errorName");
-	let errorEmail = document.getElementById("errorEmail");
-	let errorAddress = document.getElementById("errorAddress");
 	let errorLastN = document.getElementById("errorLastN");
-	let errorPassword = document.getElementById("errorPassword");
+	let errorAddress = document.getElementById("errorAddress");
 	let errorPhone = document.getElementById("errorPhone");
+	let errorEmail = document.getElementById("errorEmail");
+	let errorPassword = document.getElementById("errorPassword");
+	
 
 	// Validate fields entered by the user: name, phone, password, and email
-	debugger
-
-	const isValidPassword = fPassword.checkValidity()
-	const isValidPhone = fPhone.checkValidity()
-	const isValidLastN = fLastN.checkValidity()
-	const isValidEmail = fEmail.checkValidity()
 	const isValidName = fName.checkValidity()
+	const isValidLastN = fLastN.checkValidity()
 	const isValidAdress = fAddress.checkValidity()
-
+	const isValidPhone = fPhone.checkValidity()
+	const isValidEmail = fEmail.checkValidity()
+	const isValidPassword = fPassword.checkValidity()
+		
 
 	if(fName.value == "" || isValidName == false){
 		return error++;
 	}
 
 	if(fLastN.value == "" || isValidLastN == false){
-		error++;
+		return error++;
 	}
 
 	if(fEmail.value == "" || isValidEmail == false){
-		error++;
+		return error++;
 	}
 
 	if(fAddress.value == "" || isValidAdress == false){
-		error++;
+		return error++;
 	}
 
 
 
 	if(fPassword.value == "" || isValidPassword == false){
-		error++;
+		return error++;
 	}
 
 	if(fPhone.value == "" || isValidPhone == false){
--- js/shop.js
+++ js/shop.js
@@ -76,7 +76,7 @@ function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
     for (let i = 0; i < products.length; i++) {
-        if (cart.push(products[i].id)) {
+        if (id === products[i].id)
             cartList.push(products[i]);
         }
         console.log("cartList", cartList);
@@ -88,37 +88,100 @@ function buy(id) {
 // Exercise 2
 function cleanCart() {
 
+    cartList.splice(0, cartList.length);
+    console.log("El carrito está vacío.");
 }
 
 
 // Exercise 3
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
+    
+    let totalPrice = 0
+
     for (item of cartList) {
-        total += item.price;
+        totalPrice += item.price;
       }
-      console.log(total);
+      console.log(totalPrice);
 }
 
 // Exercise 4
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-    cartList[];
-
-
-
-
+   
+    cart.splice(0, cart.length);
+    for (let i = 0; i < cartList.length; i++) {
+		const product = cartList[i];
+		console.log("product", product);
+		const existingProduct = cart.find((item) => item.id === product.id);
+		console.log("existing", existingProduct);
+		if (existingProduct) {
+			existingProduct.quantity++;
+		} else {
+			product.quantity = 1;
+			cart.push(product);
+		}
+	}
+	console.log("cart no promotions", cart);
+	applyPromotionsCart();
+	console.log("cart promotions", cart);
+	calculateTotal();
+	document.getElementById('count_product').innerHTML= cartList.length
 }
+
 
 // Exercise 5
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+    for(let i = 0; i < cart.length; i++) {
+
+        cart[i].subtotalWithDiscount = (cart[i].price * cart[i].quantity)
+    }
+    
+    const indexOil = cart.findIndex(element => element.name == "cooking oil")
+    const indexCake = cart.findIndex(element => element.name == "Instant cupcake mixture")
+
+    if (indexOil !== -1) {
+
+        if(cart[indexOil].quantity >= 3) {
+
+            cart[indexOil].subtotalWithDiscount = (10 * cart[indexOil].quantity)
+        }
+    }
+
+    if (indexCake !== -1) {
+
+        if(cart[indexCake].quantity >= 10) {
+
+            cart[indexCake].subtotalWithDiscount = ((cart[indexCake].price * cart[indexCake].quantity) - ((cart[indexCake].price * cart[indexCake].quantity) / 3))
+        }
+    }
+    console.log(cart)
+
+
 }
 
 // Exercise 6
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
+    
+	let cartContainer = document.getElementById("cartContainer");
+    let productTemplate = document.getElementById("productTemplate");
+
+    for (let i = 0; i < cart.length; i++) {
+        let product = cart[i];
+        let total = product.subtotalWithDiscount.toFixed(2);
+
+        let productElement = productTemplate.content.importNode(true);
+  
+        productElement.querySelector(".product-name").textContent = product.name;
+        productElement.querySelector(".product-price").textContent = product.price;
+        productElement.querySelector(".product-quantity").textContent = product.quantity;
+        productElement.querySelector(".product-subtotal").textContent = total;
+
+        cartContainer.appendChild(productElement);
+    }
 }
 
 
@@ -129,12 +192,43 @@ function addToCart(id) {
     // Refactor previous code in order to simplify it 
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+    const indexCart = cart.findIndex(element => element.id == id)
+
+    if (indexCart == (-1)) {
+
+        const indexProducts = products.findIndex(element => element.id == id)
+
+        cart.push(products[indexProducts])
+
+        const foundIndexCart2 = cart.findIndex(element => element.id == id)
+
+        cart[foundIndexCart2].quantity = 1;
+
+    }
+
+    if(foundIndexCart !== (-1)) {
+
+        cart[foundIndexCart].quantity = cart[foundIndexCart].quantity + 1;
+    }
+
+    applyPromotionsCart()
+    refreshNumber()
+    
+    console.log(cart)
+   
 }
 
 // Exercise 8
 function removeFromCart(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
+    const listaCompra = cartList.findIndex(product => product.id === id);
+    if (listaCompra !== -1) {
+        cartList.splice(listaCompra, 1);
+    }
+    
+    console.log(listaCompra)
+
 }
 
 function open_modal(){
